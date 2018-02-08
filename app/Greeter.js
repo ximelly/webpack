b9ec3306b1e21d66a97313e7e14309b27119d518
Ximelly <ximelly@163.com>
2018-02-08 17:03:44 +0800

    webpack
    my first webpackDemo

---
+++ app/Greeter.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import config from './config.json';
+
+import styles from './Greeter.css';//导入
+
+
+class Greeter extends Component{
+  render() {
+    return (
+      <div className={styles.root}>{config.nickName}</div>
+    );
+  }
+}
+export default Greeter
